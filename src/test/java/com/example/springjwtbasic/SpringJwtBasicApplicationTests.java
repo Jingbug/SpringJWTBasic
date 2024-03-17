0aace42c5f84898e60257a17318cc88ba6d8d202
Jingles <rjy747@gmail.com>
2024-03-17 21:23:07 +0900

    first commit

---
+++ src/test/java/com/example/springjwtbasic/SpringJwtBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjwtbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
