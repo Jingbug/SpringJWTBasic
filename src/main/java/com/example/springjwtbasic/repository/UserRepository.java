d3e3b826765e24a02b8359fb863727ca91641377
Jingles <rjy747@gmail.com>
2024-03-17 21:59:26 +0900

    entity & repository

---
+++ src/main/java/com/example/springjwtbasic/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.springjwtbasic.repository;
+
+import com.example.springjwtbasic.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
